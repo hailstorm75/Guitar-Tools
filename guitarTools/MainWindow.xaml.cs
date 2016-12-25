da75e51e650ae8a238b433718e48e0cc12312a2e
Deno <denis.akopyan@seznam.cz>
2016-12-25 13:13:03 +0100

    Custom UI

--- guitarTools/MainWindow.xaml.cs
+++ guitarTools/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 using System.Collections.Generic;
-using guitarTools.Classes.Wheel_Select;
 using guitarTools.Classes.Fretboard;
 using guitarTools.Classes;
 
@@ -14,6 +16,7 @@ namespace guitarTools
     public partial class MainWindow : Window
     {
         Fretboard fretboard;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,6 +29,7 @@ namespace guitarTools
 
             // Creating default fretboard
             fretboard = new Fretboard(mainGrid, noteGrid, strings, frets, NoteList, 4);
+            cbRoot.SelectedIndex = 4;
 
             foreach (var item in SQLCommands.FetchList<string>("SELECT Name FROM tableScales"))
                 cbScale.Items.Add(item);
@@ -35,6 +39,50 @@ namespace guitarTools
         {
             if (fretboard.Root != cbRoot.SelectedIndex)
                 fretboard.UpdateRoot(cbRoot.SelectedIndex);
+        }
+
+        private void WindowTop_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                    Top = 0;
+                    Left = Mouse.GetPosition(this).X - ActualWidth / 2;
+                }
+
+                DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Minimized;
+        }
+
+        private void mainWindow_StateChanged(object sender, System.EventArgs e)
+        {
+            //if (WindowState == WindowState.Normal)
+            //{
+            //    DoubleAnimation da = new DoubleAnimation();
+            //    da.From = 1;
+            //    da.To = 0;
+            //    da.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+            //    MainWindow.OpacityProperty.Be
+            //}
+        }
+
+        private void MainWindow_OnActivated(object sender, EventArgs e)
+        {
+            //change the WindowStyle back to None, but only after the Window has been activated
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => WindowStyle = WindowStyle.None));
         }
     }
 }
